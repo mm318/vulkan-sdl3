2f917490120a7950d578622c942f80423ee1facf
Miaofei <ameision@hotmail.com>
2025-09-09 22:27:03 -0700

    builds and runs

--- build.zig
+++ build.zig
@@ -23,45 +23,40 @@ pub fn build(b: *std.Build) void {
     });
     exe_mod.addImport("dvui", dvui_mod);
 
-    const exe_test_mod = b.createModule(.{
-        .root_source_file = b.path("src/main.zig"),
+    if (dvui_dep.builder.lazyDependency("sdl3", .{
         .target = target,
         .optimize = optimize,
-    });
-
-    if (b.systemIntegrationOption("sdl3", .{})) {
-        exe_mod.linkSystemLibrary("SDL3", .{});
-    } else {
-        if (dvui_dep.builder.lazyDependency("sdl3", .{
-            .target = target,
-            .optimize = optimize,
-        })) |sdl3| {
-            exe_mod.linkLibrary(sdl3.artifact("SDL3"));
-        }
+    })) |sdl3| {
+        exe_mod.linkLibrary(sdl3.artifact("SDL3"));
     }
 
+    const exe = b.addExecutable(.{
+        .name = "game_of_life",
+        .root_module = exe_mod,
+    });
+    if (want_lto != null and want_lto.?) {
+        exe.lto = .thin;
+    }
+    exe.addIncludePath(b.path("src/vulkan/"));
+    exe.addCSourceFile(.{ .file = b.path("src/vulkan/vk_mem_alloc.cpp") });
+    compile_all_shaders(b, exe);
+    exe.linkLibCpp();
+    exe.linkSystemLibrary(if (target.result.os.tag == .windows) "vulkan-1" else "vulkan");
+
+    b.installArtifact(exe);
+
+    const run_cmd = b.addRunArtifact(exe);
+    // This allows the user to pass arguments to the application in the build
+    // command itself, like this: `zig build run -- arg1 arg2 etc`
+    if (b.args) |args| {
+        run_cmd.addArgs(args);
+    }
+    run_cmd.step.dependOn(b.getInstallStep());
+
     const run_step = b.step("run", "Run the app");
-        const exe = b.addExecutable(.{
-            .name = "game_of_life",
-            .root_module = exe_mod,
-        });
-        if (want_lto != null and want_lto.?) {
-            exe.lto = .thin;
-        }
+    run_step.dependOn(&run_cmd.step);
 
-        b.installArtifact(exe);
-
-        const run_cmd = b.addRunArtifact(exe);
-        // This allows the user to pass arguments to the application in the build
-        // command itself, like this: `zig build run -- arg1 arg2 etc`
-        if (b.args) |args| {
-            run_cmd.addArgs(args);
-        }
-
-        run_cmd.step.dependOn(b.getInstallStep());
-        run_step.dependOn(&run_cmd.step);
-
-    const exe_unit_tests = b.addTest(.{ .root_module = exe_test_mod });
+    const exe_unit_tests = b.addTest(.{ .root_module = exe_mod });
     const run_exe_unit_tests = b.addRunArtifact(exe_unit_tests);
 
     const test_step = b.step("test", "Run unit tests");
@@ -90,4 +85,40 @@ pub fn build(b: *std.Build) void {
         const run_bench_tests = b.addRunArtifact(bench_tests);
         test_step.dependOn(&run_bench_tests.step);
     }
+}
+
+fn compile_all_shaders(b: *std.Build, exe: *std.Build.Step.Compile) void {
+    // This is a fix for a change between zig 0.11 and 0.12
+
+    const shaders_dir = if (@hasDecl(@TypeOf(b.build_root.handle), "openIterableDir"))
+        b.build_root.handle.openIterableDir("shaders", .{}) catch @panic("Failed to open shaders directory")
+    else
+        b.build_root.handle.openDir("shaders", .{ .iterate = true }) catch @panic("Failed to open shaders directory");
+
+    var file_it = shaders_dir.iterate();
+    while (file_it.next() catch @panic("Failed to iterate shader directory")) |entry| {
+        if (entry.kind == .file) {
+            const ext = std.fs.path.extension(entry.name);
+            if (std.mem.eql(u8, ext, ".glsl")) {
+                const basename = std.fs.path.basename(entry.name);
+                const name = basename[0 .. basename.len - ext.len];
+
+                std.debug.print("Found shader file to compile: {s}. Compiling with name: {s}\n", .{ entry.name, name });
+                add_shader(b, exe, name);
+            }
+        }
+    }
+}
+
+fn add_shader(b: *std.Build, exe: *std.Build.Step.Compile, name: []const u8) void {
+    const source = std.fmt.allocPrint(b.allocator, "shaders/{s}.glsl", .{name}) catch @panic("OOM");
+    const outpath = std.fmt.allocPrint(b.allocator, "shaders/{s}.spv", .{name}) catch @panic("OOM");
+
+    const shader_compilation = b.addSystemCommand(&.{"glslangValidator"});
+    shader_compilation.addArg("-V");
+    shader_compilation.addArg("-o");
+    const output = shader_compilation.addOutputFileArg(outpath);
+    shader_compilation.addFileArg(b.path(source));
+
+    exe.root_module.addAnonymousImport(name, .{ .root_source_file = output });
 }
